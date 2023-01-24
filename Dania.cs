56a9a37e69a5946481a3c1a124368e88fa30a369
Wojtek <Wojtek@DESKTOP-1Q6ACUJ>
2023-01-24 11:45:18 +0100

    //

---
+++ Dania.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studia_programowanie_obietkowe_zaliczenie
+{
+    internal class Dania
+    {
+
+    }
+}
